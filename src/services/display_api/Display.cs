86b519945d9a761d144460a4b0475e7377e8b6e4
Henrique Reinaldi <carlosofalmeida@gmail.com>
2026-05-24 15:20:06 -0300

    cliuentfcctory

--- src/services/display_api/Display.cs
+++ src/services/display_api/Display.cs
@@ -4,47 +4,61 @@ using System.Net.Http.Json;
 
 using contracts;
 
-Console.WriteLine("===============");
-Console.WriteLine("DISPLAY DISPLAY");
-Console.WriteLine("===============");
-
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-
-app.MapGet("/sudoku", async () => {
-    var client = new HttpClient();
-    var response = await client.GetFromJsonAsync<new_sudokus>(
-        "http://localhost:5121/new"
-    );
-
-    return Results.Ok(response?.boards);
-}).WithName("GetNewBoards");
-
-
-app.MapGet("/stats/{nome}", async (string nome) => {
-    var client = new HttpClient();
-
-
-    var response = await client.GetAsync($"http://localhost:5127/stats/{nome}");
-
-    if (! response.IsSuccessStatusCode)
+class Display
+{
+    static void Main(string[] args)
     {
-        return Results.NotFound("coagulo inexistente?");
+        Console.WriteLine("===============");
+        Console.WriteLine("DISPLAY DISPLAY");
+        Console.WriteLine("===============");
+
+
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddHttpClient("sudoku_s", client => { client.BaseAddress = new Uri("http://localhost:5121/");});
+        builder.Services.AddHttpClient("bd_s", client => { client.BaseAddress = new Uri("http://localhost:5127/"); });
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        
+        IHttpClientFactory cf = app.Services.GetRequiredService<IHttpClientFactory>();
+        
+
+ 
+        app.MapGet("/sudoku", async () => {
+            HttpClient? client = cf.CreateClient("sudoku_s");
+            new_sudokus? response = await client.GetFromJsonAsync<new_sudokus>("new");
+
+            return Results.Ok(response?.boards);
+        }).WithName("GetNewBoards");
+
+        app.MapGet("/leaderboard", async () =>
+        {
+            return Results.Ok("wow");
+        });
+
+        app.MapGet("/stats/{nome}", async (string nome) => {
+            var client = cf.CreateClient("bd_s");
+
+            var response = await client.GetAsync($"/stats/{nome}");
+
+            if (! response.IsSuccessStatusCode)
+            {
+                return Results.NotFound("jogador não existe");
+            }
+
+
+            var stats = await client.GetFromJsonAsync<user_stats>($"/stats/{nome}");
+
+            return Results.Ok(stats);
+        }).WithName("GetUserStats");
+
+        app.Run();
+
     }
-
-
-    var stats = await client.GetFromJsonAsync<user_stats>(
-        $"http://localhost:5127/stats/{nome}"
-    );
-
-    return Results.Ok(stats);
-}).WithName("GetUserStats");
-
-app.Run();
+}
